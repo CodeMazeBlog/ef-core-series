e644c60ff83e1f5a9ca3079cd52099611d8b1516
Marinko <marinkospasojevic@gmail.com>
2020-01-01 16:04:39 +0100

    3.1 support

--- EFCoreApp/EFCoreApp/Entities/ApplicationContext.cs
+++ EFCoreApp/EFCoreApp/Entities/ApplicationContext.cs
@@ -1,17 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace EFCoreApp.Entities
 {
     public class ApplicationContext : DbContext
     {
-        public ApplicationContext(DbContextOptions options)
-            :base(options)
+        public ApplicationContext(DbContextOptions options) 
+            : base(options) 
         {
-
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
